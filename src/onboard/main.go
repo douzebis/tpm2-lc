5e405c63b88f5005157a040e107b6611b350abce
Frederic Ruget <frederic.ruget@thalesgroup.com>
2023-02-20 19:16:58 +0100

    -

--- src/onboard/main.go
+++ src/onboard/main.go
@@ -345,7 +345,7 @@ func CreateAK(rwc io.ReadWriter) {
 	if err != nil {
 		glog.Fatalf("akTpmPublicKey.Encode() failed: %v", err)
 	}
-	glog.V(10).Infof("EkPub %v", ekPubBytes)
+	glog.V(10).Infof("EkPub %v", ekPublicKeyDer)
 	glog.V(10).Infof("AkName %v", akName)
 	glog.V(10).Infof("AkPub %v", akPubBytes)
 
