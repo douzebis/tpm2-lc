4e401548935a4c50918aff69bb5c21df43950dbf
Frederic Ruget <frederic.ruget@thalesgroup.com>
2023-02-24 13:26:01 +0100

    -

--- src/steps/verify-ek-pub.go
+++ src/steps/verify-ek-pub.go
@@ -3,28 +3,10 @@
 package steps
 
 import (
-	"bytes"
-	"crypto/rand"
-	"crypto/rsa"
-	"crypto/x509"
-	"crypto/x509/pkix"
-	"encoding/asn1"
-	"encoding/pem"
-	"flag"
 	"io"
-	"io/ioutil"
-	"log"
-	"math/big"
-	"time"
-
-	"github.com/golang/glog"
-	"github.com/google/go-tpm-tools/client"
-	"github.com/google/go-tpm/tpm2"
-
-	"main/src/certs"
-	"main/src/steps"
 )
 
 // ### Clear TPM (on Attestor) #################################################
 
-func Clear(rwc io.ReadWriter) {
+func VerifyEKPub(rwc io.ReadWriter) {
+}
