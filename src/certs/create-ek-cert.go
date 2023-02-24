d5bf9dcc78c0f41d0a2168ed384712c9ceac14c6
Frederic Ruget <frederic.ruget@thalesgroup.com>
2023-02-24 16:43:49 +0100

    -

---
+++ src/certs/create-ek-cert.go
@@ -0,0 +1,124 @@
+// SPDX-License-Identifier: Apache-2.0
+
+package certs
+
+import (
+	"crypto/rand"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/asn1"
+	"encoding/pem"
+	"fmt"
+	"math/big"
+	"time"
+
+	"main/src/lib"
+)
+
+// === Create TPM EK certificate ===============================================
+
+func CreateEKCert(
+	// See https://upgrades.intel.com/content/CRL/ekcert/EKcertPolicyStatement.pdf
+	// See https://marc.info/?l=openssl-users&m=135119943225986&w=2
+	publicKey interface{},
+	ManufacturerID string,
+	ModelName string,
+	Version string,
+	caCert x509.Certificate,
+	caPrivKey interface{},
+	filePrefix string,
+) {
+	now := time.Now()
+	certTemplate := x509.Certificate{
+		SerialNumber: big.NewInt(1),
+		Subject:      pkix.Name{},
+		NotBefore:    now,
+		NotAfter:     now.AddDate(10, 0, 0),
+		KeyUsage:     x509.KeyUsageKeyEncipherment,
+		ExtraExtensions: []pkix.Extension{
+			*CreateSubjectAltName(
+				[]byte(ManufacturerID), // "id: Google"
+				[]byte(ModelName),      // "id: Shielded VM vTPM"
+				[]byte(Version),        // "id: 00010001"
+			),
+		},
+		BasicConstraintsValid: true,
+		IsCA:                  false,
+	}
+
+	certBytes, err := x509.CreateCertificate(
+		rand.Reader,
+		&certTemplate,
+		&caCert,
+		publicKey,
+		caPrivKey)
+	if err != nil {
+		lib.Fatal("x509.CreateCertificate() failed: %v", err)
+	}
+
+	// pem encode
+	certPEM := []byte(pem.EncodeToMemory(
+		&pem.Block{
+			Type:  "CERTIFICATE",
+			Bytes: certBytes,
+		},
+	))
+
+	lib.Write(fmt.Sprintf("%s.crt", filePrefix), certPEM, 0644) // "TPM-CA/tpm"
+
+	// --- Verify TPM cert -----------------------------------------------------
+
+	// Note: equivalently with openssl:
+	// openssl verify -CAfile TPM-CA/tpm-ca.crt TPM-CA/tpm.crt
+	// openssl x509 -noout -ext subjectAltName -in TPM-CA/tpm.crt
+
+	cert, err := x509.ParseCertificate(certBytes)
+	if err != nil {
+		lib.Fatal("x509.ParseCertificate() failed: %v", err)
+	}
+	cert.UnhandledCriticalExtensions = []asn1.ObjectIdentifier{}
+
+	roots := x509.NewCertPool()
+	roots.AddCert(&caCert)
+	opts := x509.VerifyOptions{
+		Roots: roots,
+	}
+
+	if _, err := cert.Verify(opts); err != nil {
+		lib.Fatal("tpmCert.Verify() failed: %v", err)
+	} else {
+		lib.Print("Verified %s", "TPM-CA/tpm.crt")
+	}
+
+}
+
+// --- Snippet: parse a certificate extensions -----------------------------
+
+//	testPem, err := ioutil.ReadFile("TPM-CA/tpm.crt")
+//	if err != nil {
+//		lib.Fatal("ioutil.ReadFile() failed: %v", err)
+//	}
+//	block, _ := pem.Decode([]byte(testPem))
+//	if block == nil {
+//		lib.Fatal("pem.Decode() failed: %v", err)
+//	}
+//
+//	if block.Type == "CERTIFICATE" {
+//		lib.Print("Block has type CERTIFICATE")
+//		certificate, err := x509.ParseCertificate(block.Bytes)
+//		if err != nil {
+//			lib.Fatal("x509.ParseCertificate() failed: %v", err)
+//		}
+//		for _, ext := range certificate.Extensions {
+//			// filter the custom extensions by customOID
+//			lib.Print("extension %s", ext.Id.String())
+//			if ext.Id.String() == "2.5.29.17" {
+//				parse(ext.Value, "")
+//			}
+//		}
+//	} else {
+//		lib.Print("Block has type %s", block.Type)
+//	}
+
+// Since GCP Shielded VMs TPM Endorsement Keys come without a proper
+// certificate, we fake a TPM CA and a fake TPM EK certificate.
--- src/init/main.go
+++ src/init/main.go
@@ -3,20 +3,7 @@
 package main
 
 import (
-	"crypto/rand"
-	"crypto/rsa"
-	"crypto/x509"
-	"crypto/x509/pkix"
-	"encoding/asn1"
-	"encoding/pem"
 	"flag"
-	"io/ioutil"
-	"math/big"
-	"time"
-
-	"github.com/golang/glog"
-	"github.com/google/go-tpm-tools/client"
-	"github.com/google/go-tpm/tpm2"
 
 	"main/src/certs"
 	"main/src/steps"
@@ -31,9 +18,25 @@ var (
 func main() {
 	flag.Parse()
 
+	// Create certificate for Manufacturer CA
+	certs.CreateCA(
+		"Manufacturer",
+		"Manufacturer/manufacturer-ca",
+	)
+
+	// Create certificate for Owner CA
+	//manufacturerCert, manufacturerPrivKey :=
+	certs.CreateCA(
+		"Owner",
+		"Owner/owner-ca",
+	)
+
 	// Open TPM
 	rwc := tpm.OpenFlush(*tpmPath, *flush)
 	defer rwc.Close()
+
+	// In this mock-up, we fake boot image PCRs prediction by simply
+	// reading current machine PCRs status
 
 	// Read and save TPM PCRs values
 	tpm.ReadPCRs(
@@ -43,142 +46,128 @@ func main() {
 	)
 
 	// Read and save TPM EK Pub
-	ekPublicKey, ekPubBytes := steps.GetEKPub(
+	//ekPublicKey, ekPubBytes :=
+	steps.GetEKPub(
 		rwc,
 		"TPM-CA/ek",
 	)
 
-	// Create certificate for TPM CA
-	tpmCaCert, tpmCaPrivKey := certs.CreateCA(
-		"TPM Manufacturer",
-		"TPM-CA/tpm-ca",
-	)
-
 	return
 
-	// === Create certificate for TPM ==========================================
-
-	// --- Retrieve TPM EK Pub -------------------------------------------------
-
-	ekTpmKey, err := client.EndorsementKeyRSA(rwc)
-	if err != nil {
-		glog.Fatalf("Unable to load SRK from TPM: %v", err)
-	}
-
-	ekTpmPubKey, _, _, err := tpm2.ReadPublic(rwc, ekTpmKey.Handle())
-	if err != nil {
-		glog.Fatalf("tpm2.ReadPublic() failed: %s", err)
-	}
-
-	ekPubKey, err := ekTpmPubKey.Key()
-	if err != nil {
-		glog.Fatalf("ekPublicKey.Key() failed: %s", err)
-	}
-	ekPubBytes, err = x509.MarshalPKIXPublicKey(ekPubKey)
-	if err != nil {
-		glog.Fatalf("x509.MarshalPKIXPublicKey() failed: %v", err)
-	}
-
-	ekPubPEM := pem.EncodeToMemory(
-		&pem.Block{
-			Type:  "PUBLIC KEY",
-			Bytes: ekPubBytes,
-		},
-	)
-
-	err = ioutil.WriteFile("TPM-CA/ek.pem", ekPubPEM, 0644)
-	if err != nil {
-		glog.Fatalf("ioutil.WriteFile() failed: %v", err)
-	}
-
-	glog.V(0).Infof("Wrote TPM-CA/ek.pem")
-
-	switch ekPubKey.(type) {
-	case *rsa.PublicKey:
-		glog.V(0).Infof("ekPublicKey is of type RSA")
-	}
-	// From https://stackoverflow.com/a/44317246
-	ekPublicKey, _ = ekPubKey.(*rsa.PublicKey)
-
-	// --- Create TPM EK certificate -------------------------------------------
-
-	tpmTemplate := x509.Certificate{
-		SerialNumber: big.NewInt(1),
-		Subject: pkix.Name{
-			Organization: []string{"TPM Inc"},
-			CommonName:   "TPM",
-		},
-		NotBefore: time.Now(),
-		NotAfter:  time.Now().AddDate(10, 0, 0),
-		KeyUsage:  x509.KeyUsageKeyEncipherment,
-		ExtraExtensions: []pkix.Extension{
-			*certs.CreateSubjectAltName(
-				[]byte("id: Google"),
-				[]byte("id: Shielded VM vTPM"),
-				[]byte("id: 00010001"),
-			),
-		},
-		BasicConstraintsValid: true,
-		IsCA:                  false,
-	}
-
-	tpmBytes, err := x509.CreateCertificate(
-		rand.Reader,
-		&tpmTemplate,
-		tpmCaCert,
-		ekPublicKey,
-		tpmCaPrivKey)
-	if err != nil {
-		glog.Fatalf("x509.CreateCertificate() failed: %v", err)
-	}
-
-	// pem encode
-	tpmPEM := []byte(pem.EncodeToMemory(
-		&pem.Block{
-			Type:  "CERTIFICATE",
-			Bytes: tpmBytes,
-		},
-	))
-
-	err = ioutil.WriteFile("TPM-CA/tpm.crt", tpmPEM, 0644)
-	if err != nil {
-		glog.Fatalf("ioutil.WriteFile() failed: %v", err)
-	}
-
-	glog.V(0).Infof("Wrote TPM-CA/tpm.crt")
-
-	// --- Verify TPM cert -----------------------------------------------------
-
-	// Note: equivalently with openssl:
-	// openssl verify -CAfile TPM-CA/tpm-ca.crt TPM-CA/tpm.crt
-	// openssl x509 -noout -ext subjectAltName -in TPM-CA/tpm.crt
-
-	tpmCert, err := x509.ParseCertificate(tpmBytes)
-	if err != nil {
-		glog.Fatalf("x509.ParseCertificate() failed: %v", err)
-	}
-	tpmCert.UnhandledCriticalExtensions = []asn1.ObjectIdentifier{}
-
-	roots := x509.NewCertPool()
-	roots.AddCert(tpmCaCert)
-	opts := x509.VerifyOptions{
-		Roots: roots,
-	}
-
-	if _, err := tpmCert.Verify(opts); err != nil {
-		glog.Fatalf("tpmCert.Verify() failed: %v", err)
-	} else {
-		glog.V(0).Infof("Verified %s", "TPM-CA/tpm.crt")
-	}
-
-	// === Create certificate for Owner CA =====================================
-
-	certs.CreateCA("TPM Owner", "Owner-CA/owner-ca")
-
-	// === Retrieve PCRs =======================================================
-
-	// In this tutorial, we fake boot image PCRs prediction by simply
-	// reading current machine PCRs status
+	//	// === Create certificate for TPM ==========================================
+	//
+	//	// --- Retrieve TPM EK Pub -------------------------------------------------
+	//
+	//	ekTpmKey, err := client.EndorsementKeyRSA(rwc)
+	//	if err != nil {
+	//		glog.Fatalf("Unable to load SRK from TPM: %v", err)
+	//	}
+	//
+	//	ekTpmPubKey, _, _, err := tpm2.ReadPublic(rwc, ekTpmKey.Handle())
+	//	if err != nil {
+	//		glog.Fatalf("tpm2.ReadPublic() failed: %s", err)
+	//	}
+	//
+	//	ekPubKey, err := ekTpmPubKey.Key()
+	//	if err != nil {
+	//		glog.Fatalf("ekPublicKey.Key() failed: %s", err)
+	//	}
+	//	ekPubBytes, err = x509.MarshalPKIXPublicKey(ekPubKey)
+	//	if err != nil {
+	//		glog.Fatalf("x509.MarshalPKIXPublicKey() failed: %v", err)
+	//	}
+	//
+	//	ekPubPEM := pem.EncodeToMemory(
+	//		&pem.Block{
+	//			Type:  "PUBLIC KEY",
+	//			Bytes: ekPubBytes,
+	//		},
+	//	)
+	//
+	//	err = ioutil.WriteFile("TPM-CA/ek.pem", ekPubPEM, 0644)
+	//	if err != nil {
+	//		glog.Fatalf("ioutil.WriteFile() failed: %v", err)
+	//	}
+	//
+	//	glog.V(0).Infof("Wrote TPM-CA/ek.pem")
+	//
+	//	switch ekPubKey.(type) {
+	//	case *rsa.PublicKey:
+	//		glog.V(0).Infof("ekPublicKey is of type RSA")
+	//	}
+	//	// From https://stackoverflow.com/a/44317246
+	//	ekPublicKey, _ = ekPubKey.(*rsa.PublicKey)
+	//
+	//	// --- Create TPM EK certificate -------------------------------------------
+	//
+	//	tpmTemplate := x509.Certificate{
+	//		SerialNumber: big.NewInt(1),
+	//		Subject: pkix.Name{
+	//			Organization: []string{"TPM Inc"},
+	//			CommonName:   "TPM",
+	//		},
+	//		NotBefore: time.Now(),
+	//		NotAfter:  time.Now().AddDate(10, 0, 0),
+	//		KeyUsage:  x509.KeyUsageKeyEncipherment,
+	//		ExtraExtensions: []pkix.Extension{
+	//			*certs.CreateSubjectAltName(
+	//				[]byte("id: Google"),
+	//				[]byte("id: Shielded VM vTPM"),
+	//				[]byte("id: 00010001"),
+	//			),
+	//		},
+	//		BasicConstraintsValid: true,
+	//		IsCA:                  false,
+	//	}
+	//
+	//	tpmBytes, err := x509.CreateCertificate(
+	//		rand.Reader,
+	//		&tpmTemplate,
+	//		tpmCaCert,
+	//		ekPublicKey,
+	//		tpmCaPrivKey)
+	//	if err != nil {
+	//		glog.Fatalf("x509.CreateCertificate() failed: %v", err)
+	//	}
+	//
+	//	// pem encode
+	//	tpmPEM := []byte(pem.EncodeToMemory(
+	//		&pem.Block{
+	//			Type:  "CERTIFICATE",
+	//			Bytes: tpmBytes,
+	//		},
+	//	))
+	//
+	//	err = ioutil.WriteFile("TPM-CA/tpm.crt", tpmPEM, 0644)
+	//	if err != nil {
+	//		glog.Fatalf("ioutil.WriteFile() failed: %v", err)
+	//	}
+	//
+	//	glog.V(0).Infof("Wrote TPM-CA/tpm.crt")
+	//
+	//	// --- Verify TPM cert -----------------------------------------------------
+	//
+	//	// Note: equivalently with openssl:
+	//	// openssl verify -CAfile TPM-CA/tpm-ca.crt TPM-CA/tpm.crt
+	//	// openssl x509 -noout -ext subjectAltName -in TPM-CA/tpm.crt
+	//
+	//	tpmCert, err := x509.ParseCertificate(tpmBytes)
+	//	if err != nil {
+	//		glog.Fatalf("x509.ParseCertificate() failed: %v", err)
+	//	}
+	//	tpmCert.UnhandledCriticalExtensions = []asn1.ObjectIdentifier{}
+	//
+	//	roots := x509.NewCertPool()
+	//	roots.AddCert(tpmCaCert)
+	//	opts := x509.VerifyOptions{
+	//		Roots: roots,
+	//	}
+	//
+	//	if _, err := tpmCert.Verify(opts); err != nil {
+	//		glog.Fatalf("tpmCert.Verify() failed: %v", err)
+	//	} else {
+	//		glog.V(0).Infof("Verified %s", "TPM-CA/tpm.crt")
+	//	}
 
 }
 
